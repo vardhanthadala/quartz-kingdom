283d4d16b92eec9db98e5c17bf037c32de8d766a
Vardhan <thadalavardhangoud@gmail.com>
2026-04-21 16:16:10 +0530

    fixed nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -44,12 +44,15 @@ export default function Navbar({ threshold = 50, initialHidden = false }) {
         //       : 'text-white opacity-100'
         //   }`}
         className={`fixed top-0 left-0 w-full z-[1000] transition-all duration-500 
-px-6 md:px-12  flex justify-between items-center 
+px-6 md:px-12 flex justify-between items-center
+
+h-[60px] md:h-[55px] lg:h-[65px] xl:h-[65px] 2xl:h-[68px]
+
 ${isScrolled
-  ? 'bg-white/80 backdrop-blur-lg shadow-sm py-3 md:py-3 xl:py-6 text-black'
+  ? 'bg-white/80 backdrop-blur-lg shadow-sm h-[56px] md:h-[58px] lg:h-[60px] xl:h-[64px] 2xl:h-[68px] text-black'
   : initialHidden
-    ? 'opacity-0 pointer-events-none text-white py-4 md:py-4'
-    : 'text-white py-4 md:py-4'
+    ? 'opacity-0 pointer-events-none text-white'
+    : 'text-white'
 }`}
       >
         <Link href="/" className="group relative z-[1001] drop-shadow-[0_2px_8px_rgba(0,0,0,0.3)]">
